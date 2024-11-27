525c9302dd13ae0526bef43d7fbeec26506baebc
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2024-11-27 09:15:03 -0800

    fix: follow wow casing convention

--- SimpleNotes.lua
+++ SimpleNotes.lua
@@ -1,8 +1,8 @@
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
 
-frame:SetScript("OnEvent", function(self, event, addon_name)
-    if event == "ADDON_LOADED" and addon_name == "SimpleNotes" then
+frame:SetScript("OnEvent", function(self, event, addonName)
+    if event == "ADDON_LOADED" and addonName == "SimpleNotes" then
         if CharacterNotes == nil then
             CharacterNotes = ""
         end
