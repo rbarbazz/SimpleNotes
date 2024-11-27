7f3e2c0347472771a0e4839392acab1f8de06238
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2024-11-27 09:33:00 -0800

    refactor: move onload code to function

--- SimpleNotes.lua
+++ SimpleNotes.lua
@@ -1,10 +1,18 @@
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("ADDON_LOADED")
+local simpleNotesFrame = CreateFrame("Frame")
+simpleNotesFrame:RegisterEvent("ADDON_LOADED")
 
-frame:SetScript("OnEvent", function(self, event, addonName)
-    if event == "ADDON_LOADED" and addonName == "SimpleNotes" then
-        if CharacterNotes == nil then
-            CharacterNotes = ""
-        end
+local function handleAddonLoaded(addonName)
+    if addonName ~= "SimpleNotes" then
+        return
+    end
+
+    if CharacterNotes == nil then
+        CharacterNotes = ""
+    end
+end
+
+simpleNotesFrame:SetScript("OnEvent", function(self, event, arg1)
+    if event == "ADDON_LOADED" then
+        return handleAddonLoaded(arg1)
     end
 end)
