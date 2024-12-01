f99aa511180a5416b46d2713d8ee922459103af8
rbarbazz <42906704+rbarbazz@users.noreply.github.com>
2024-12-01 04:46:00 -0800

    Create backend fns with slash commands

--- SimpleNotes.lua
+++ SimpleNotes.lua
@@ -1,3 +1,4 @@
+-- Event Handlers
 local simpleNotesFrame = CreateFrame("Frame")
 simpleNotesFrame:RegisterEvent("ADDON_LOADED")
 
@@ -16,3 +17,23 @@ simpleNotesFrame:SetScript("OnEvent", function(self, event, arg1)
         return handleAddonLoaded(arg1)
     end
 end)
+
+-- Helper Functions
+local function saveNotes(notes)
+    CharacterNotes = notes
+end
+
+local function getNotes()
+    return CharacterNotes or ""
+end
+
+local function printNotes()
+    print(getNotes())
+end
+
+-- Slash Commands
+SLASH_SIMPLENOTESSAVE1 = "/sns"
+SLASH_SIMPLENOTESPRINT1 = "/snp"
+
+SlashCmdList["SIMPLENOTESSAVE"] = saveNotes
+SlashCmdList["SIMPLENOTESPRINT"] = printNotes
